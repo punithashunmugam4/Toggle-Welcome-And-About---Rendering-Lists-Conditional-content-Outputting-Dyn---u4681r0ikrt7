20723065537804a36faaf94c3ac1183e01e2584a
punithashunmugam4 <punithashunmugam4@gmail.com>
2022-10-12 13:35:31 +0530

    first

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 import "../styles/App.css";
 
 const App = () => {
+  const [style1,setstyle1]=useState({display:"block"})
+  const [style2,setstyle2]=useState({display:"none"})
+  const display=()=>{
+    if(style1.display==="block"){
+      setstyle1({display:"none"})
+      setstyle2({display:"block"})
+    }
+    else{
+      setstyle1({display:"block"})
+      setstyle2({display:"none"})
+    }
+    }
   return (
     <div id="main">
-      <div id="welcome-div">
+      <div id="welcome-div" style={style1}>
         <h1>Welcome to Newton School!!</h1>
       </div>
-      <div id="about-div">
+      <div id="about-div" style={style2}>
         <h1>
           Newton School is a neo-university providing highly immersive tech
           learning to millions of students, to tap into new-age tech
           opportunities.
         </h1>
       </div>
-      <button id="toggle">Toggle</button>
+      <button id="toggle" onClick={display}>Toggle</button>
     </div>
   );
 };
