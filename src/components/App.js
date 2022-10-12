33971e138d8220493752030e14eca5586e296875
punithashunmugam4 <punithashunmugam4@gmail.com>
2022-10-12 15:32:19 +0530

    second

--- src/components/App.js
+++ src/components/App.js
@@ -4,16 +4,34 @@ import "../styles/App.css";
 const App = () => {
   const [style1,setstyle1]=useState({display:"block"})
   const [style2,setstyle2]=useState({display:"none"})
-  const display=()=>{
+  const handleClick=(event)=>{
     if(style1.display==="block"){
-      setstyle1({display:"none"})
-      setstyle2({display:"block"})
+      
     }
     else{
-      setstyle1({display:"block"})
-      setstyle2({display:"none"})
+      
     }
+    switch (event.detail) {
+      case 1: {
+        console.log('single click');
+        setstyle1({display:"none"})
+        setstyle2({display:"block"})
+        break;
+      }
+      case 2: {
+        console.log('double click');
+        setstyle1({display:"block"})
+       setstyle2({display:"none"})
+        break;
+      }
+      case 3: {
+        console.log('triple click');
+        setstyle1({display:"block"})
+        setstyle2({display:"none"})
+        break;
+      }
     }
+  }
   return (
     <div id="main">
       <div id="welcome-div" style={style1}>
@@ -26,9 +44,9 @@ const App = () => {
           opportunities.
         </h1>
       </div>
-      <button id="toggle" onClick={display}>Toggle</button>
+      <button id="toggle" onClick={handleClick}>Toggle</button>
     </div>
   );
 };
 
-export default App;
+export default App
